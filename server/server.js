9d496668ac941a44608b507649961ae11943fd95
Traspo <nyaspo8@gmail.com>
2024-02-27 22:05:54 -0500

    updating mongodb

--- server/server.js
+++ server/server.js
@@ -15,20 +15,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
